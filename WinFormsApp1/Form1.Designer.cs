ea16343514872aa57ea96d1b87f9a17601918690
Bejinariu-Alexandra-Georgiana <59794804+Bejinariu-Alexandra-Georgiana@users.noreply.github.com>
2021-08-18 15:11:17 +0300

    Add files via upload

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.Updatebutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(44, 12);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 51;
+            this.dataGridViewParent.RowTemplate.Height = 29;
+            this.dataGridViewParent.Size = new System.Drawing.Size(559, 290);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(746, 12);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 51;
+            this.dataGridViewChild.RowTemplate.Height = 29;
+            this.dataGridViewChild.Size = new System.Drawing.Size(555, 290);
+            this.dataGridViewChild.TabIndex = 1;
+            // 
+            // Updatebutton
+            // 
+            this.Updatebutton.Location = new System.Drawing.Point(614, 430);
+            this.Updatebutton.Name = "Updatebutton";
+            this.Updatebutton.Size = new System.Drawing.Size(92, 41);
+            this.Updatebutton.TabIndex = 2;
+            this.Updatebutton.Text = "Update";
+            this.Updatebutton.UseVisualStyleBackColor = true;
+            this.Updatebutton.Click += new System.EventHandler(this.Updatebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 526);
+            this.Controls.Add(this.Updatebutton);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.Button Updatebutton;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString =  ConfigurationManager.ConnectionStrings["WinFormsApp1.Properties.Settings.Setting"].ConnectionString;
+        DataSet ds = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder cmdbl;
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show(connection.State.ToString());
+                    adapter.SelectCommand = new SqlCommand("SELECT * FROM Sectie", connection);
+                    adapter.Fill(ds, "Sectie");
+                    dataGridViewParent.DataSource = ds.Tables["Sectie"];
+                    SqlDataAdapter childAdapter = new SqlDataAdapter("SELECT * FROM Doctor;", connection);
+                    childAdapter.Fill(ds, "Doctor");
+                    BindingSource parentBS = new BindingSource();
+                    BindingSource childBS = new BindingSource();
+                    parentBS.DataSource = ds.Tables["Sectie"];
+                    dataGridViewParent.DataSource = parentBS;
+                    DataColumn parentPK = ds.Tables["Sectie"].Columns["cod_s"];
+                    DataColumn childFK = ds.Tables["Doctor"].Columns["cod_s"];
+                    DataRelation relation = new DataRelation("fk_parent_child", parentPK, childFK);
+                    ds.Relations.Add(relation);
+                    childBS.DataSource = parentBS;
+                    childBS.DataMember = "fk_parent_child";
+                    dataGridViewChild.DataSource = childBS;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void Updatebutton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlDataAdapter childAdapter = new SqlDataAdapter(ConfigurationManager.AppSettings["populatingCT"], connection);
+                    cmdbl = new SqlCommandBuilder(childAdapter);
+                    childAdapter.Update(ds, ConfigurationManager.AppSettings["childTable"]);
+                    MessageBox.Show("Information updated", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+    }
+}
